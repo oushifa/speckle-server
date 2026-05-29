a09d52055b7c3e2679accc91435baceeba833185
ukinll <ukinll@163.com>
2026-05-29 15:21:51 +0800

    feat:dwg图纸转换

--- packages/server/modules/shared/helpers/envHelper.ts
+++ packages/server/modules/shared/helpers/envHelper.ts
@@ -566,14 +566,16 @@ export function getOdaUserSecret() {
 
 export function getOdaBaseUrl() {
   // return getStringFromEnv('ODA_BASE_URL', { default: 'http://127.0.0.1:8089' })
-  const defaultBaseUrl = isProdEnv()
-    ? 'http://speckle-server-dwg2dxf-1:8080'
-    : 'http://127.0.0.1:8089'
-
-  console.log(defaultBaseUrl, 'defaultBaseUrl')
   return getStringFromEnv('ODA_BASE_URL', {
-    default: defaultBaseUrl
+    default: 'http://speckle-server-dwg2dxf-1:8080'
   })
+  // const defaultBaseUrl = isProdEnv()
+  //   ? 'http://speckle-server-dwg2dxf-1:8080'
+  //   : 'http://127.0.0.1:8089'
+  // console.log(defaultBaseUrl, 'defaultBaseUrl')
+  // return getStringFromEnv('ODA_BASE_URL', {
+  //   default: defaultBaseUrl
+  // })
 }
 
 export const areSavedViewsEnabled = (): boolean =>
